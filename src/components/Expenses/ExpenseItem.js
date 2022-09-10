add1e58dba0f002630539abe12aa8cbddb04589e
Theoutrace <prakashkumar.imw@gmail.com>
2022-09-11 04:37:54 +0530

    delete button Added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,35 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
-
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
 
+  const clickDeleteHandler = () =>{
+    console.log('targetToDelete');
+    
+  }
+
+  const clickHandler = () => {
+    setTitle('updated');
+    // console.log(title);
+  };
 
   return (
     <Card className="expense-item">
-    <ExpenseDate date={props.date}/>
-      <Card className="expense-item__description">
-        <ExpenseDetails title = {props.title} amount = {props.amount}/>
-        <h4>{props.LocationOfExpenditure}</h4>
-      </Card>
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <ExpenseDetails title={title} amount={props.amount} />
+        {/* <h4>{props.LocationOfExpenditure}</h4> */}
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={clickDeleteHandler}>Delete Expense</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
 
