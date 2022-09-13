f685edbd8f596cbf55718a436e35f2f2d86d077f
Theoutrace <prakashkumar.imw@gmail.com>
2022-09-14 03:00:34 +0530

    Added filter yearwise.

--- src/components/Expenses/ExpenseTotal.js
+++ src/components/Expenses/ExpenseTotal.js
@@ -1,19 +1,26 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
+import Filter from "../Filter/Filter";
 import "./ExpenseTotal.css";
 
 const ExpenseTotal = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
       <div className="app-expense-all-item-container">
-        {props.items.map((itm) => (
-          <ExpenseItem
-            key = {itm.id}
-            exp={itm}
-          />
+        <Filter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem key={expense.id} exp={expense} />
         ))}
       </div>
     </div>
